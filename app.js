6da055a79d2c8e8d586924e2c2a6ef6e2e9a4b57
Shaurya Dhadwal <shauryadhadwal@gmail.com>
2018-02-16 14:59:14 +0530

    Removed Views

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ const expressJwt = require('express-jwt');
 require('dotenv').config();
 
 // ROUTES
-const index = require('./routes/index');
-const users = require('./routes/users');
 const api = require('./routes/api');
 
 // MONGOOSE
@@ -27,8 +25,8 @@ const options = {
     // If not connected, return errors immediately rather than waiting for reconnect
     bufferMaxEntries: 0
 };
-mongoose.connect(uri,options);
- // Use bluebird
+mongoose.connect(uri, options);
+// Use bluebird
 mongoose.Promise = require('bluebird');
 
 const app = express();
@@ -36,39 +34,35 @@ app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 //ROUTES
-app.use('/', index);
-app.use('/users', users);
 app.use('/api', api);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
-  });
-  
+});
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
