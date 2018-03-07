30327c5fd42f35d773ff50bfe97f05041bde2f0c
Shaurya Dhadwal <shauryadhadwal@gmail.com>
2018-03-07 10:30:04 +0530

    change: models

--- models/project.js
+++ models/project.js
@@ -4,9 +4,9 @@ var Schema = mongoose.Schema;
 var Schema = new Schema({
     projectId: { type: Number, required: true, unique: true },
     name: { type: String, required: true, trim: true },
-    startDate: { type: Number },
+    portfolioId: { type: Number },   // Same as manager Id
     teamLeadId: { type: Number },
-    portfolioId: { type: Number }   // Same as manager Id
+    startDate: { type: Number }
 });
 
 module.exports = mongoose.model('Project', Schema, 'projects');
